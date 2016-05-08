89f8596d516b7c4cf87f792fe1f4bc1ba70693aa
qyxcumt <qyxcumt@gmail.com>
2016-05-08 17:15:16 +0800

    主页君背后的页面……
    通知和规定使用<a>标签href属性跳转，房源使用在<tr>标签里使用onclick跳转

--- index.php
+++ index.php
@@ -1,97 +1,149 @@
 <?php 
 require_once 'browser.php';
-session_start();
-require_once 'defines.php';
+require_once 'core/defines.php';
+require_once 'core/House.php';
+require_once 'core/notification.php';
+require_once 'core/rule.php';
 
-require_once 'Information.class.php';
-require_once 'HouseSources.class.php';
 if(isset($_POST['fun'])){
 	$fun=$_POST['fun'];
 	switch($fun){
-		case 'getHouseDiv':
-			index::getHouseDiv();
-			break;
-		case 'getNotificationDiv':
-			index::getNotificationDiv();
-			break;
-		case 'getNotificationTable':
-			index::getNotificationTable();
-			break;
-		case 'getIndex':
-			index::getHouseDiv();
-			index::getNotificationDiv();
+		case 'getInfo':
+			getInfo(); 
 			break;
 		case 'getHouse':
-			index::getHouse();
+			getHouse();
 	}
 }
 
+function getInfo(){
+	echo "<table style='min-width:250px;width:100%;' border=0 rules=none >
+		<tr><td style=\"font-size:40px;color:#eb6100;width:55%;\">最新通知
+			<a class=\"more\" href=\"notification.php\" title=\"更多通知\">···</a></td>
+		</tr>";
+			$noti=new Notification();
+			$noticount=$noti->getNotificationCount();
+			if($noticount!=0){
+				$st=$noti->getNotificationLimit(0,5);
+				for($counter=0;$counter<count($st);$counter++){
+					echo "
+						<tr>
+							<td><a class='tablelinktext' href=\"notification.html?action=detail&id=".$st[$counter][0]."\">".$st[$counter][4]."</a></td>
+						</tr>";
+				}
+			}else echo "<tr><td class=\"tablenolinktext\">暂无通知</td></tr>";
+		echo "
+		<tr>
+			<td><hr/></td>
+		</tr>
+		<tr><td  style=\"font-size:40px;color:#eb6100\">相关政策
+			<a class=\"more\" href=\"rule.php\" title=\"更多政策\">···</a></td>";
+			$rule=new rule();
+			$noticount=$rule->getRuleCount();
+			if($noticount!=0){
+				$st=$rule->getRuleLimit(0,10);
+				for($counter=0;$counter<count($st);$counter++){
+					echo "
+						<tr>
+							<td><a class='tablelinktext' href=\"notification.html?id=".$st[$counter][0]."\">".$st[$counter][4]."</a></td>
+						</tr>";
+				}
+			}else echo "<tr><td class=\"tablenolinktext\">暂无数据</td></tr>";
+		echo "
+		</tr>
+	</table>";
+}
 
-	function getNotificationDiv(){
-		index::getNotificationDiv();
-	}
-	
-	function getNotificationTable(){
-		index::getNotificationTable();
-	}
-	
-	class index{
-		static function getHouseDiv(){
-			echo "<div id=\"HouseDiv\" style=\"float:left; width:72%;margin:125px 10px 30px 10px;padding:10px 40px 0 40px;background-color:#F9F9F9;box-shadow:0 0 40px -20px rgba(0,0,0,0.3);\" >
-			<div id=\"HouseTableDiv\" style=\"margin-top:0px;color:#000;\">";
-
+function getHouse(){
+	$st=checkedHouse::getHouseTable(0, 10);
+// 	echo count($st);
+// 	die();
+	if(0==count($st))
+		echo "<p style=\"text-align:center;font-size:40px;color:#eb6100;\">对不起，暂无通过审核的房源</p>";
+	else{
+		echo "
+					<table id='HouseTable'>
+								<tr style='height:20px'>
+								<td style='width:15%'>
+									<div class='houselist'>
+										<div style='font-size:16px;'>户型</div>
+				   					</div>
+								</td>
+								<td style='width:10%'>
+									<div class='houselist'>
+										<div style='font-size:16px;'>面积</div>
+									</div>
+								</td>
+								<td style='width:10%'>
+									<div class='houselist'>
+				 						<div style='font-size:16px;'>价格(万)</div>
+									</div>
+						 		</td>
+								<td style='width:15%x'>
+									<div class='houselist'>
+										<div style='font-size:16px;'>电话</div>
+									</div>
+								</td>
+								<td style='width:15%'>
+									<div class='houselist'>
+										<div style='font-size:16px;'>联系人</div>
+									</div>
+								</td>
+								<td  style='width:17%'>
+									<div class='houselist'>
+										<div style='font-size:16px;'>地址</div>
+									</div>
+								</td>
+								<td  style='width:18%'>
+									<div class='houselist'>
+										<div style='font-size:16px;'>发布时间</div>
+				 					</div>
+								</td>
+							</tr>
+					";
+		for($counter=0;$counter<count($st);$counter++){
+			$house=new HouseDataAnalyser($st[$counter]);
 			echo "
-			</div>
-			</div>";
-			echo "<script >getHouseCount();</script>";
-		}
+						<tr onclick=\"javascript:window.location.href='house.html?POD=".$house->getPOD_NO()."style='height:0px'>
+							<td style='width:15%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".(string)($house->getRoomCount())."室".$house->getHallCount()."厅".$house->getToiletCount()."卫"."</div>
+								</div>
+							</td>
+							<td style='width:10%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".$house->getArea()."</div>
+								</div>
+							</td>
+							<td style='width:10%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".$house->getPrice()."</div>
+								</div>
+							</td>
+							<td style='width:15%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".$house->getPhone()."</div>
+								</div>
+							</td>
+							<td style='width:15%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".$house->getLinkman()."</div>
+								</div>
+							</td>
+							<td style='width:17%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".$house->getAddress()."</div>
+								</div>
+							</td>
+							<td style='width:18%'>
+								<div class='houselistdata'>
+									<div style='font-size:16px;height:20px'>".$house->getTime()."</div>
+								</div>
+							</td>
+						</tr>";
 		
-		static function getNotificationDiv(){
-			echo "<div id=\"NotificationDiv\" style=\"float:left; width:25%;margin:125px 10px 30px 10px;padding:10px 40px 0 40px;background-color:#F9F9F9;box-shadow:0 0 40px -20px rgba(0,0,0,0.3);\">
-		<div style=\"margin-top:20px;color:#000;\">";
-			index::getNotificationTable();
-			echo "</div>
-         </div>";
-		}
-		
-		static function getNotificationTable(){
-			echo "<table style='min-width:250px;width:100%;' border=0 rules=none >
-				<tr><td style=\"font-size:40px;color:#eb6100;width:55%;\">最新通知
-					<a class=\"more\" onclick=\"sjmpto('news.php?style=noteList')\" title=\"更多通知\">···</a></td>
-				</tr>";
-					$noti=new Notification();
-					$noticount=$noti->getDataCount();
-					if($noticount!=0){
-						$st=$noti->getDataLimit(0,5);
-						for($counter=0;$counter<count($st);$counter++){
-							$analyser=new NotificationAnalyser($st[$counter]);
-							echo "
-								<tr>
-									<td><a class='tablelinktext'>".$analyser->getTitle()."</a></td>
-								</tr>";
-						}
-					}else echo "<tr><td>暂无通知</td></tr>";
-				echo "
-				<tr>
-					<td><hr/></td>
-				</tr>
-				<tr><td  style=\"font-size:40px;color:#eb6100\">相关政策
-					<a class=\"more\" onclick=\"sjmpto('news.php?style=noteList')\" title=\"更多政策\">···</a></td>";
-					$noti=new Rule();
-					$noticount=$noti->getDataCount();
-					if($noticount!=0){
-						$st=$noti->getDataLimit(0,10);
-						for($counter=0;$counter<count($st);$counter++){
-							$analyser=new RuleAnalyser($st[$counter]);
-							echo "
-								<tr>
-									<td>".$analyser->getTitle()."</td>
-								</tr>";
-						}
-					}else echo "<tr><td>暂无数据</td></tr>";
-				echo "
-				</tr>
-			</table>";
 		}
 	}
+}
+
 ?>
